057d53499aea584c06f9b2634366d3a22094e724
amiapple <amitakabe@gmail.com>
2020-10-22 01:58:54 -0700

    編集ルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'top#index'
   resources :list, only: [:create, :new, :edit, :update, :destroy] do
-    resources :card, only: [:create, :new, :show]
+    resources :card, only: [:create, :new, :show, :edit, :update]
   end
 end
